fcb99881e5d71d41ef3fa900312d5be7b7e5c6a6
+ <+>
2023-10-06 11:52:17 -0400

    About done

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -16,7 +16,7 @@ const About = () => {
 
   return (
     <main id="about" className="container p-3 py-4 p-lg-0">
-      <div className="about-container px-3 py-4 p-lg-0">
+      <div className="about-container px-3 py-4 p-lg-0 d-flex align-items-stretch border border-white">
         <div className="company row m-0 mb-4">
           <div className="col-12 p-3 py-4 p-xl-5 m-0">
             <h2 className="jung-quote text-center p-0 fst-italic">
@@ -24,15 +24,18 @@ const About = () => {
               <span className="float-end">Calvin Coolidge</span>
             </h2>
           </div>
-          <div className="col-12 p-0 pe-lg-3 mb-4 mb-lg-0">
+          <div className="col-12 col-lg-6 col-xl-4">
+            <img
+              src={pavlina}
+              alt="Pavlina Proevska"
+              className="img-fluid mb-4"
+              style={{ maxWidth: "400px", maxHeight: "400px" }}
+            />
+          </div>
+          <div className="col-12 col-lg-6 col-xl-8  ps-lg-4 mb-4 mb-lg-0  justify-content-end">
             <p className="mb-2 mb-xl-2">
-              <img
-                src={pavlina}
-                alt="Pavlina Proevska"
-                className="d-block h-100 w-50 float-left"
-              />
-              A native of Macedonia,{" "}
-              <strong className="fs-5">Pavlina Proevska</strong> studied Public
+              A native of Macedonia,
+              <strong className="fs-5"> Pavlina Proevska</strong> studied Public
               Relations in Paris and worked in the news department at With the
               CBS News 1984' Summer Olympics Team as well as in the field, for
               USA Cable Networks. The highlights of her tenure at With the CBS
@@ -63,10 +66,52 @@ const About = () => {
 
           <div className="col-12 p-0 pe-lg-3 mb-4 mb-lg-0">
             <h2 className="text-center mb-4 fst-italic">
-             "Production - A combination of persistence ... and blind faith."
+              "Production - A combination of persistence ... and blind faith."
             </h2>
           </div>
-          <div className="col-12 col-lg-6 col-xl-4  p-0 pe-lg-3 mb-4 mb-lg-0">
+
+          <div className="col-12 col-lg-6 col-xl-8  ps-lg-4 mb-4 mb-lg-0">
+            <p className="mb-2 mb-xl-2">
+              <strong className="fs-5">PAVLINA Ltd.</strong> is a New York based
+              independent production company engaged in the development and
+              production of feature films and documentaries. It was incorporated
+              in 1986 by Pavlina Proevska, President and General Partner. The
+              co-producing partner of Pavlina Ltd., FilmsZaNas DOOEL is based in
+              The Republic of Macedonia.
+            </p>
+            <p className="mb-2 mb-xl-2">
+              The first feature film produced by Pavlina Ltd. in 1987 was{" "}
+              <span className="fst-italic">The Magic Snowman</span>, a live
+              action children's fairy tale starring Roger Moore as the voice of
+              the Magic Snowman. The film's gross receipts to date are ten times
+              the original low-budget cost. Distributed through Miramax/Buena
+              Vista International it has been seen by twenty million children in
+              the United States.
+            </p>
+            <p className="mb-2 mb-xl-2">
+              The second feature produced in 1992 was the supernatural thriller
+              <span className="fst-italic">Happy Hell Night</span>, starring
+              Darren McGavin. The film was distributed worldwide by several
+              international distributors and released on DVD by AnchorBay
+              entertainment. More recently, the legendary Samuel Goldwyn Films
+              acquired the rights of distribution for North America.
+            </p>
+            <p className="mb-2 mb-xl-2">
+              To ensure a successful distribution life and minimize investment
+              risks, the company focuses on development and production of
+              perennial stories. In keeping with such business strategy, over
+              the past ten years the company has financed the acquisition and
+              development of several children's properties.
+            </p>
+            <p>
+              The company's strength is the original content of the stories it
+              develops and produced and while through its feature films it
+              offers popular entertainment, the documentaries celebrate people
+              whose lives have left an imprint on the human experience.
+            </p>
+          </div>
+
+          <div className="col-12 col-lg-6 col-xl-4 p-0 ms-0 pe-lg-3 mb-4 mb-lg-0 ">
             <div
               id="AboutCarousel"
               className="carousel slide carousel-fade"
@@ -130,46 +175,6 @@ const About = () => {
                 <span className="visually-hidden">Next</span>
               </button>
             </div>
-          </div>
-          <div className="col-12 col-lg-6 col-xl-8  p-0 ps-lg-4 mb-4 mb-lg-0">
-            <p className="mb-2 mb-xl-2">
-              <strong className="fs-5">PAVLINA Ltd.</strong> is a New York based
-              independent production company engaged in the development and
-              production of feature films and documentaries. It was incorporated
-              in 1986 by Pavlina Proevska, President and General Partner. The
-              co-producing partner of Pavlina Ltd., FilmsZaNas DOOEL is based in
-              The Republic of Macedonia.
-            </p>
-            <p className="mb-2 mb-xl-2">
-              The first feature film produced by Pavlina Ltd. in 1987 was{" "}
-              <span className="fst-italic">The Magic Snowman</span>, a live
-              action children's fairy tale starring Roger Moore as the voice of
-              the Magic Snowman. The film's gross receipts to date are ten times
-              the original low-budget cost. Distributed through Miramax/Buena
-              Vista International it has been seen by twenty million children in
-              the United States.
-            </p>
-            <p className="mb-2 mb-xl-2">
-              The second feature produced in 1992 was the supernatural thriller
-              <span className="fst-italic">Happy Hell Night</span>, starring
-              Darren McGavin. The film was distributed worldwide by several
-              international distributors and released on DVD by AnchorBay
-              entertainment. More recently, the legendary Samuel Goldwyn Films
-              acquired the rights of distribution for North America.
-            </p>
-            <p className="mb-2 mb-xl-2">
-              To ensure a successful distribution life and minimize investment
-              risks, the company focuses on development and production of
-              perennial stories. In keeping with such business strategy, over
-              the past ten years the company has financed the acquisition and
-              development of several children's properties.
-            </p>
-            <p>
-              The company's strength is the original content of the stories it
-              develops and produced and while through its feature films it
-              offers popular entertainment, the documentaries celebrate people
-              whose lives have left an imprint on the human experience.
-            </p>
           </div>
         </div>
       </div>
